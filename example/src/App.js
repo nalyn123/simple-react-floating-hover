97f210485375bedf5cc145846500c3740255ccbb
Rona <taguiamronalyn123@gmail.com>
2025-10-04 17:16:31 +0800

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'simple-react-floating-hover'
+import 'simple-react-floating-hover/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
